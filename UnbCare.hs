00f7332efe88c699983c8e76f7f589f7ae172a5c
Thiago-AS <thiagos.araujo09@gmail.com>
2019-04-23 13:12:14 -0300

    Adding removemeds function

--- UnbCare.hs
+++ UnbCare.hs
@@ -8,13 +8,19 @@ type Medicamentos = [Medicamento]
 type Prescricao = (Nome,Horario,HorarioProximo)
 type PlanoMedicamento = [Prescricao]
 
-isInList :: Medicamento -> Medicamentos -> Bool
+isInList :: Nome -> Medicamentos -> Bool
 isInList med [] = False
 isInList med (x:xs) 
- | fst med == fst x = True
+ | med == fst x = True
  | otherwise = isInList med xs
 
 adicionarMedicamento :: Medicamento -> Medicamentos -> Medicamentos
 adicionarMedicamento newMed meds 
- | isInList newMed meds = map (\x -> if fst x == fst newMed then (fst x, snd x + snd newMed) else x) meds 
- | otherwise = newMed: meds 
+ | isInList (fst newMed) meds = map (\x -> if fst x == fst newMed then (fst x, snd x + snd newMed) else x) meds 
+ | otherwise = newMed: meds
+
+removerMedicamento :: Nome -> Medicamentos -> Medicamentos
+removerMedicamento name [] = []
+removerMedicamento name meds
+ | isInList name meds = filter (\x -> if fst x /= name then True else False) meds
+ | otherwise = meds
